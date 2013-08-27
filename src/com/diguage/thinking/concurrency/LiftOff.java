23ff66347e1a2c0f434e35bc1755040bfaecd2a3
diguage <leejun119@gmail.com>
2013-08-27 15:17:50 +0800

    增加示例程序

---
+++ src/com/diguage/thinking/concurrency/LiftOff.java
@@ -0,0 +1,34 @@
+package com.diguage.thinking.concurrency;
+
+/**
+ * Runnable接口示例
+ * <p/>
+ * User: D瓜哥，http://www.diguage.com/
+ * Date: 13-8-27
+ * Time: 下午3:08
+ */
+public class LiftOff implements Runnable {
+    protected int countDown = 10;
+    private static int taskCount = 0;
+    private final int id = taskCount++;
+
+    public LiftOff() {
+    }
+
+    public LiftOff(int countDown) {
+        this.countDown = countDown;
+    }
+
+    public String status() {
+        return "#" + id + "(" +
+                (countDown > 0 ? countDown : "LiftOff!") + ")";
+    }
+
+    @Override
+    public void run() {
+        while (countDown-- > 0) {
+            System.out.println(status());
+            Thread.yield();
+        }
+    }
+}
