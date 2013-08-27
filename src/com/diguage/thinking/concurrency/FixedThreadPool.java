456f19f0873284ce4e8843f3ad3c8d1159a9a5b2
diguage <leejun119@gmail.com>
2013-08-27 15:48:12 +0800

    FixedThreadPool示例

--- src/com/diguage/thinking/concurrency/FixedThreadPool.java
+++ src/com/diguage/thinking/concurrency/FixedThreadPool.java
@@ -1,9 +1,20 @@
 package com.diguage.thinking.concurrency;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
+ * FixedThreadPool
  * User: D瓜哥，http://www.diguage.com/
  * Date: 13-8-27
  * Time: 下午3:33
  */
 public class FixedThreadPool {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new LiftOff());
+        }
+        exec.shutdown();
+    }
 }
