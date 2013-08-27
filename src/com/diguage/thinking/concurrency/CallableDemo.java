19ec4357beaef1aaa960e8e427506a8c94aad051
diguage <leejun119@gmail.com>
2013-08-28 00:06:41 +0800

    增加Callable接口演示

---
+++ src/com/diguage/thinking/concurrency/CallableDemo.java
@@ -0,0 +1,47 @@
+package com.diguage.thinking.concurrency;
+
+import java.util.ArrayList;
+import java.util.concurrent.*;
+
+/**
+ * User: D瓜哥，http://www.diguage.com/
+ * Date: 13-8-27
+ * Time: 下午4:57
+ */
+
+class TaskWithResult implements Callable<String> {
+    private int id;
+
+    public TaskWithResult(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String call() throws Exception {
+        return "result of TaskWithResult " + id;
+    }
+}
+
+public class CallableDemo {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        ArrayList<Future<String>> results = new ArrayList<Future<String>>();
+
+        for (int i = 0; i < 10; i++) {
+            results.add(exec.submit(new TaskWithResult(i)));
+        }
+
+        for (Future<String> fs : results) {
+            try {
+                System.out.println(fs.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return;
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } finally {
+                exec.shutdown();
+            }
+        }
+    }
+}
