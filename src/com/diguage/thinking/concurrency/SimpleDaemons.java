ae34ffcb1068da5d27b6c6d04ebf3de5117b7787
diguage <leejun119@gmail.com>
2013-08-30 11:23:23 +0800

    简单后台线程示例

--- src/com/diguage/thinking/concurrency/SimpleDaemons.java
+++ src/com/diguage/thinking/concurrency/SimpleDaemons.java
@@ -1,9 +1,34 @@
 package com.diguage.thinking.concurrency;
 
+import java.util.concurrent.TimeUnit;
+
 /**
+ * 后台线程示例
+ * <p/>
  * User: D瓜哥，http://www.diguage.com/
  * Date: 13-8-30
  * Time: 上午10:32
  */
-public class SimpleDaemons {
+public class SimpleDaemons implements Runnable {
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                TimeUnit.MILLISECONDS.sleep(100);
+                System.out.println(Thread.currentThread() + " " + this);
+            }
+        } catch (InterruptedException e) {
+            System.out.print("sleep() interrupted");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            Thread daemon = new Thread(new SimpleDaemons());
+            daemon.setDaemon(true);
+            daemon.start();
+        }
+        System.out.println("All daemons started");
+        TimeUnit.MILLISECONDS.sleep(175);
+    }
 }
