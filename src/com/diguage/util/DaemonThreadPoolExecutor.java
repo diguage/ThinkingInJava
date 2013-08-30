bff511562c60805e677dd4c07015ad9f80736330
diguage <leejun119@gmail.com>
2013-08-30 12:03:49 +0800

    后台线程定义执行器

--- src/com/diguage/util/DaemonThreadPoolExecutor.java
+++ src/com/diguage/util/DaemonThreadPoolExecutor.java
@@ -1,9 +1,18 @@
 package com.diguage.util;
 
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * User: D瓜哥，http://www.diguage.com/
  * Date: 13-8-30
  * Time: 上午11:34
  */
-public class DaemonThreadPoolExecutor {
+public class DaemonThreadPoolExecutor extends ThreadPoolExecutor {
+    public DaemonThreadPoolExecutor() {
+        super(0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS,
+                new SynchronousQueue<Runnable>(),
+                new DaemonThreadFactory());
+    }
 }
