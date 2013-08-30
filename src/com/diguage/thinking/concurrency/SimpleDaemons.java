3799f520edf99d0433dad7b7d4dc1b616f808728
diguage <leejun119@gmail.com>
2013-08-30 11:19:57 +0800

    创建后台线程工厂类

---
+++ src/com/diguage/thinking/concurrency/SimpleDaemons.java
@@ -0,0 +1,9 @@
+package com.diguage.thinking.concurrency;
+
+/**
+ * User: D瓜哥，http://www.diguage.com/
+ * Date: 13-8-30
+ * Time: 上午10:32
+ */
+public class SimpleDaemons {
+}
---
+++ src/com/diguage/util/DaemonThreadFactory.java
@@ -0,0 +1,9 @@
+package com.diguage.util;
+
+/**
+ * User: D瓜哥，http://www.diguage.com/
+ * Date: 13-8-30
+ * Time: 上午10:57
+ */
+public class DaemonThreadFactory {
+}
