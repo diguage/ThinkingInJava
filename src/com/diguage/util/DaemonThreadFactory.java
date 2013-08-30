cf1328ddb60cf18314b4b8b9109f9575ff1f911b
diguage <leejun119@gmail.com>
2013-08-30 11:33:32 +0800

    更正后台线程类的实现

--- src/com/diguage/util/DaemonThreadFactory.java
+++ src/com/diguage/util/DaemonThreadFactory.java
@@ -1,9 +1,19 @@
 package com.diguage.util;
 
+import java.util.concurrent.ThreadFactory;
+
 /**
+ * 后台线程工厂类
+ * <p/>
  * User: D瓜哥，http://www.diguage.com/
  * Date: 13-8-30
  * Time: 上午10:57
  */
-public class DaemonThreadFactory {
+public class DaemonThreadFactory implements ThreadFactory {
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(r);
+        t.setDaemon(true);
+        return t;
+    }
 }
