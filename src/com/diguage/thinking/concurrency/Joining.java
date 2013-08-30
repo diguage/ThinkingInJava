349b4b29c33d82c45ad6b1765ad003be9a6468c5
diguage <leejun119@gmail.com>
2013-08-30 16:04:39 +0800

    join()方法的使用

---
+++ src/com/diguage/thinking/concurrency/Joining.java
@@ -0,0 +1,59 @@
+package com.diguage.thinking.concurrency;
+
+/**
+ * User: D瓜哥，http://www.diguage.com/
+ * Date: 13-8-30
+ * Time: 下午2:38
+ */
+class Sleeper extends Thread {
+    private int duration;
+
+    public Sleeper(String name, int sleepTime) {
+        super(name);
+        duration = sleepTime;
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            sleep(duration);
+        } catch (InterruptedException e) {
+            System.out.println(getName() + " was interrupted. " +
+                    "isInturrupted(): " + isInterrupted());
+            return;
+        }
+        System.out.println(getName() + "has awakened");
+    }
+}
+
+class Joiner extends Thread {
+    private Sleeper sleeper;
+
+    public Joiner(String name, Sleeper sleeper) {
+        super(name);
+        this.sleeper = sleeper;
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            sleeper.join();
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println(getName() + " join completed");
+    }
+}
+
+public class Joining {
+    public static void main(String[] args) {
+        Sleeper sleepy = new Sleeper("Sleepy", 1500),
+                grumpy = new Sleeper("Grumpy", 1500);
+
+        Joiner dopey = new Joiner("Dopey", sleepy),
+                doc = new Joiner("Doc", grumpy);
+        grumpy.interrupt();
+    }
+}
