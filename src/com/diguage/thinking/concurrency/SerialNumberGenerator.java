02836201e0238942acf1642d56cf2686e3748053
diguage <leejun119@gmail.com>
2013-09-03 16:13:53 +0800

    演示volatile关键字在累加时，不保证线程安全性

---
+++ src/com/diguage/thinking/concurrency/SerialNumberGenerator.java
@@ -0,0 +1,17 @@
+package com.diguage.thinking.concurrency;
+
+/**
+ * 演示volatile关键字在累加时，不保证线程安全性
+ * <p/>
+ * User: D瓜哥，http://www.diguage.com/
+ * Date: 13-9-3
+ * Time: 下午3:56
+ */
+public class SerialNumberGenerator extends IntGenerator {
+    private static volatile int serialNumber = 0;
+
+    @Override
+    public int next() {
+        return serialNumber++;  // mot thread-safe
+    }
+}
