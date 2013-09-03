5fe43dbac3cc2c2601382e21a2105e2466cb4d43
diguage <leejun119@gmail.com>
2013-09-03 11:08:08 +0800

    演示线程异常的捕获。

---
+++ src/com/diguage/thinking/concurrency/CaptureUncaughtException.java
@@ -0,0 +1,47 @@
+package com.diguage.thinking.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * User: D瓜哥，http://www.diguage.com/
+ * Date: 13-9-3
+ * Time: 上午10:15
+ */
+
+class ExecptionThread2 implements Runnable {
+    @Override
+    public void run() {
+        Thread t = Thread.currentThread();
+        System.out.println(" run() by " + t);
+        System.out.println(" en = " + t.getUncaughtExceptionHandler());
+        throw new RuntimeException();
+    }
+}
+
+class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.println("caught " + e);
+    }
+}
+
+class HandlerThreadFactory implements ThreadFactory {
+    @Override
+    public Thread newThread(Runnable r) {
+        System.out.println(this + " creating new Thread");
+        Thread t = new Thread(r);
+        System.out.println("created " + t);
+        t.setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+        System.out.println("eh = " + t.getUncaughtExceptionHandler());
+        return t;
+    }
+}
+
+public class CaptureUncaughtException {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool(new HandlerThreadFactory());
+        exec.execute(new ExecptionThread2());
+    }
+}
