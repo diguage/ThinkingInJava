57f8eebbfcc92fbf95f6b9d412f2ae26875b728b
diguage <leejun119@gmail.com>
2013-10-01 19:29:24 +0800

    CyclicBarrier示例

---
+++ src/com/diguage/books/thinking/concurrency/HorseRace.java
@@ -0,0 +1,116 @@
+package com.diguage.books.thinking.concurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * CyclicBarrier示例
+ * <p/>
+ * Coder: D瓜哥，http://www.diguage.com/
+ * Date: 2013-10-01
+ * Time: 19:06
+ */
+
+class Horse implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private int strides = 0;
+    private static Random random = new Random(47);
+    private static CyclicBarrier barrier;
+
+    Horse(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
+    public synchronized int getStrides() {
+        return strides;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                synchronized (this) {
+                    strides += random.nextInt(3);
+                }
+                barrier.await();
+            }
+        } catch (InterruptedException e) {
+            // A legitimate way to exit
+        } catch (BrokenBarrierException e) {
+            // This one we want to know about
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Horse " + id + " ";
+    }
+
+    public String tracks() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < getStrides(); i++) {
+            sb.append("*");
+        }
+        sb.append(id);
+        return sb.toString();
+    }
+}
+
+public class HorseRace {
+    static final int FINISH_LINE = 75;
+    private List<Horse> horses = new ArrayList<Horse>();
+    private ExecutorService exec = Executors.newCachedThreadPool();
+    private CyclicBarrier barrier;
+
+    public HorseRace(int nHorses, final int pause) {
+        barrier = new CyclicBarrier(nHorses, new Runnable() {
+            @Override
+            public void run() {
+                StringBuilder s = new StringBuilder();
+                for (int i = 0; i < FINISH_LINE; i++) {
+                    s.append("=");
+                }
+                System.out.println(s);
+                for (Horse horse : horses) {
+                    System.out.println(horse.tracks());
+                }
+                for (Horse horse : horses) {
+                    if (horse.getStrides() >= FINISH_LINE) {
+                        System.out.println(horse + " won!");
+                        exec.shutdownNow();
+                        return;
+                    }
+                }
+                try {
+                    TimeUnit.MILLISECONDS.sleep(pause);
+                } catch (InterruptedException e) {
+                    System.out.println("barrier-action sleep interrupted");
+                }
+            }
+        });
+        for (int i = 0; i < nHorses; i++) {
+            Horse horse = new Horse(barrier);
+            horses.add(horse);
+            exec.execute(horse);
+        }
+    }
+
+    public static void main(String[] args) {
+        int nHorses = 7;
+        int pause = 200;
+        if (args.length > 0) {
+            int n = new Integer(args[0]);
+            nHorses = n > 0 ? n : nHorses;
+        }
+        if (args.length > 1) {
+            int p = new Integer(args[1]);
+            pause = p > -1 ? p : pause;
+        }
+
+        new HorseRace(nHorses, pause);
+    }
+}
