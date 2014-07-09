883cd7a092659aee0c7791b67c0846e7f9759e3d
D瓜哥 <leejun119@gmail.com>
2014-07-09 15:35:22 +0800

    io.DirLIst

---
+++ src/com/diguage/books/thinking/io/DirList.java
@@ -0,0 +1,40 @@
+package com.diguage.books.thinking.io;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.regex.Pattern;
+
+/**
+ * 使用正则表达式来展示某一目录下的文件。
+ * <p/>
+ * Coder：D瓜哥，http://www.diguage.com/
+ * <p/>
+ * Date: 2014-07-09 15:21
+ */
+public class DirList {
+    public static void main(String[] args) {
+        File path = new File(".");
+        String[] list = null;
+        if (args.length == 0) {
+            list = path.list();
+        } else {
+            list = path.list(new DirFilter(args[0]));
+        }
+        for (String dirItem : list) {
+            System.out.println(dirItem);
+        }
+    }
+}
+
+class DirFilter implements FilenameFilter {
+    private Pattern pattern;
+
+    public DirFilter(String regex) {
+        this.pattern = Pattern.compile(regex);
+    }
+
+    @Override
+    public boolean accept(File dir, String name) {
+        return this.pattern.matcher(name).matches();
+    }
+}
