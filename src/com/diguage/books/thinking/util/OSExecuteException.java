c7e07f2e38a5aa6efe572950c26ef3bb74038816
D瓜哥 <leejun119@gmail.com>
2014-07-12 10:01:23 +0800

    util.OSExecuteException

---
+++ src/com/diguage/books/thinking/util/OSExecuteException.java
@@ -0,0 +1,13 @@
+package com.diguage.books.thinking.util;
+
+/**
+ * <p/>
+ * Coder：D瓜哥，http://www.diguage.com/
+ * <p/>
+ * Date: 2014-07-12 10:00
+ */
+public class OSExecuteException extends RuntimeException {
+    public OSExecuteException(String message) {
+        super(message);
+    }
+}
